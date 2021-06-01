a084dc3927c5a4f75d216123402ae8c559820fc1
gratienj <jean-marc.gratien@ifpen.fr>
2021-06-01 14:02:51 +0200

    update FindMCGSolver.cmake

--- packages/FindMCGSolver.cmake
+++ packages/FindMCGSolver.cmake
@@ -10,7 +10,7 @@
 # MCGSOLVER_LIBRARIES
 #
 # Target mcgsolver
- 
+
 if(NOT MCGSOLVER_ROOT)
   set(MCGSOLVER_ROOT $ENV{MCGSOLVER_ROOT})
 endif()
@@ -21,7 +21,8 @@ else()
   set(_MCGSOLVER_SEARCH_OPTS)
 endif()
 
-if(NOT MCGSOLVER_FOUND) 
+if(NOT MCGSOLVER_FOUND)
+  MESSAGE("MCGSOLVER_ROOT ${MCGSOLVER_ROOT}")
 
   find_library(MCGSOLVER_LIBRARY 
     NAMES MCGSolver
@@ -30,7 +31,9 @@ if(NOT MCGSOLVER_FOUND)
 		${_MCGSOLVER_SEARCH_OPTS}
     )
   mark_as_advanced(MCGSOLVER_LIBRARY)
-  
+
+  MESSAGE("MCGSOLVER_LIBRARY ${MCGSOLVER_LIBRARY}")
+
   find_library(MCGSOLVER_CUDA_LIBRARY 
     NAMES MCGSolverCUDA
 		HINTS ${MCGSOLVER_ROOT}
@@ -39,7 +42,7 @@ if(NOT MCGSOLVER_FOUND)
     )
   mark_as_advanced(MCGSOLVER_CUDA_LIBRARY)
   
-  find_path(MCGSOLVER_INCLUDE_DIR MCGSolver/MCGSolver.h
+  find_path(MCGSOLVER_INCLUDE_DIR MCGS.h
     HINTS ${MCGSOLVER_ROOT} 
 		PATH_SUFFIXES include
     ${_MCGSOLVER_SEARCH_OPTS}
@@ -49,7 +52,7 @@ if(NOT MCGSOLVER_FOUND)
 endif()
 
 # pour limiter le mode verbose
-set(MCGSOLVER_FIND_QUIETLY ON)
+set(MCGSOLVER_FIND_QUIETLY OFF)
 
 find_package_handle_standard_args(MCGSOLVER
 	DEFAULT_MSG 
@@ -58,16 +61,18 @@ find_package_handle_standard_args(MCGSOLVER
   MCGSOLVER_CUDA_LIBRARY)
 
 if(MCGSOLVER_FOUND AND NOT TARGET mcgsolver)
-    
+  
   set(MCGSOLVER_INCLUDE_DIRS ${MCGSOLVER_INCLUDE_DIR})
   
   set(MCGSOLVER_LIBRARIES ${MCGSOLVER_LIBRARY}
-                          ${MCGSOLVER_CUDA_LIBRARY})
+    ${MCGSOLVER_CUDA_LIBRARY})
   
+  set(MCGSOLVER_FLAGS USE_HWLOC USE_CUDA)
+
   # mcgsolver main
-	  
+	
   add_library(mcgsolver_main UNKNOWN IMPORTED)
-	  
+	
   set_target_properties(mcgsolver_main PROPERTIES 
 	  INTERFACE_INCLUDE_DIRECTORIES "${MCGSOLVER_INCLUDE_DIRS}")
   
@@ -75,10 +80,10 @@ if(MCGSOLVER_FOUND AND NOT TARGET mcgsolver)
     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
     IMPORTED_LOCATION "${MCGSOLVER_LIBRARY}")
   
-  # domain decomp
+  # mcgsolver cuda
   
 	add_library(mcgsolver_cuda UNKNOWN IMPORTED)
-	  
+	
 	set_target_properties(mcgsolver_cuda PROPERTIES
     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
     IMPORTED_LOCATION "${MCGSOLVER_CUDA_LIBRARY}")
@@ -86,7 +91,7 @@ if(MCGSOLVER_FOUND AND NOT TARGET mcgsolver)
   # mcgsolver
   
 	add_library(mcgsolver INTERFACE IMPORTED)
-	  
+	
 	set_property(TARGET mcgsolver APPEND PROPERTY 
     INTERFACE_LINK_LIBRARIES "mcgsolver_main")
 
@@ -94,6 +99,6 @@ if(MCGSOLVER_FOUND AND NOT TARGET mcgsolver)
     INTERFACE_LINK_LIBRARIES "mcgsolver_cuda")
 
   set_target_properties(mcgsolver PROPERTIES
-    INTERFACE_COMPILE_DEFINITIONS "CUDA_DP;KERNEL_DP")
+    INTERFACE_COMPILE_DEFINITIONS "${MCGSOLVER_FLAGS}")
 
 endif()
