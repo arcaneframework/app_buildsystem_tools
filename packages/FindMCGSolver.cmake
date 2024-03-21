db744deb50f71cd136c8ed9954513fda7263e12f
guignont <thomas.guignon@ifpen.fr>
2024-03-21 15:11:49 +0100

    - update FindMCGSolver for master

--- packages/FindMCGSolver.cmake
+++ packages/FindMCGSolver.cmake
@@ -42,7 +42,7 @@ if(NOT MCGSOLVER_FOUND)
     )
     mark_as_advanced(MCGSOLVER_CUDA_LIBRARY)
 
-    find_path(MCGSOLVER_INCLUDE_DIR MCGSolver/ILinearSolver.h
+    find_path(MCGSOLVER_INCLUDE_DIR MCGSolver/MCGSolver.h
             HINTS ${MCGSOLVER_ROOT}
             PATH_SUFFIXES include
             ${_MCGSOLVER_SEARCH_OPTS}
