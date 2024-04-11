9b008d3759584efc1ec8773087d129b9c412b327
stdcm <stephane.de-chaisemartin@ifpen.fr>
2024-04-11 12:51:02 +0200

    [packages] Update new finder for Linux.

--- packages/FindNeo.cmake
+++ packages/FindNeo.cmake
@@ -1,14 +1,13 @@
 # Find Neo::Neo target : only needed to copy dll. Must set NEO_LIBRARIES
-if (WIN32)
-    # Neo depends on SGraph (Header only library)
-    find_package(SGraph REQUIRED)
-    find_package(Neo REQUIRED)
 
-    # set NEO_LIBRARIES
-    if (CMAKE_BUILD_TYPE)
-        string(TOUPPER "${CMAKE_BUILD_TYPE}" UPPER_BUILD_TYPE)
-        get_target_property(NEO::NEO_LIBRARIES Neo::Neo IMPORTED_IMPLIB_${UPPER_BUILD_TYPE})
-    else()
-        get_target_property(NEO::NEO_LIBRARIES Neo::Neo IMPORTED_IMPLIB)
-    endif()
-endif ()
+# Neo depends on SGraph (Header only library)
+find_package(SGraph REQUIRED)
+find_package(Neo REQUIRED)
+
+# set NEO_LIBRARIES
+if (CMAKE_BUILD_TYPE)
+    string(TOUPPER "${CMAKE_BUILD_TYPE}" UPPER_BUILD_TYPE)
+    get_target_property(NEO::NEO_LIBRARIES Neo::Neo IMPORTED_IMPLIB_${UPPER_BUILD_TYPE})
+else()
+    get_target_property(NEO::NEO_LIBRARIES Neo::Neo IMPORTED_IMPLIB)
+endif()
--- packages/FindZoltan.cmake
+++ packages/FindZoltan.cmake
@@ -1,10 +1,9 @@
 # For Windows only : to copy Zoltan dll
 # ZOLTAN_LIBRARIES must contain the complete name (path included) of Zoltan.lib
 
-if (WIN32)
-    find_package(Zoltan QUIET)
-    if (Zoltan_FOUND)
-        STRING(CONCAT ZOLTAN_LIBRARIES_FULLNAME ${Zoltan_LIBRARY_DIRS} "/" ${Zoltan_LIBRARIES} ".lib")
-        set (ZOLTAN_LIBRARIES ${ZOLTAN_LIBRARIES_FULLNAME})
-    endif ()
+find_package(Zoltan QUIET)
+if (Zoltan_FOUND AND WIN32)
+    STRING(CONCAT ZOLTAN_LIBRARIES_FULLNAME ${Zoltan_LIBRARY_DIRS} "/" ${Zoltan_LIBRARIES} ".lib")
+    set (ZOLTAN_LIBRARIES ${ZOLTAN_LIBRARIES_FULLNAME})
 endif ()
+
