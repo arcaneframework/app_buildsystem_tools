224f12ccb9cab7c944583e75e5639116eb8a7ed6
sdcm <stephane.de-chaisemartin@ifpen.fr>
2025-06-18 14:56:27 +0200

    [Config,install] Allow ArcGeoSim config file to be found with find_package in install configuration.

--- arcgeosim/LoadArcGeoSim.cmake
+++ arcgeosim/LoadArcGeoSim.cmake
@@ -6,7 +6,7 @@
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
 
-cmake_minimum_required(VERSION 3.7.1)
+cmake_minimum_required(VERSION 3.13)
 
 # pour VERSION dans la commande project
 cmake_policy(SET CMP0048 NEW)
@@ -20,6 +20,7 @@ set(INFRA_BUILDSYSTEM_PATH ${ARCGEOSIM_FRAMEWORK_ROOT}/ArcaneInfra/build-system)
 set(ARCGEOSIM_BUILD_SYSTEM_PATH ${ARCGEOSIM_FRAMEWORK_ROOT}/ArcaneInfra/build-system) 
 list(APPEND CMAKE_MODULE_PATH ${INFRA_BUILDSYSTEM_PATH}/tools)
 list(APPEND CMAKE_MODULE_PATH ${INFRA_BUILDSYSTEM_PATH})
+list(APPEND CMAKE_PREFIX_PATH ${ARCGEOSIM_FRAMEWORK_ROOT})
 
 # ----------------------------------------------------------------------------
 # ----------------------------------------------------------------------------
