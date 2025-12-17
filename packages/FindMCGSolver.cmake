291859098e4949ba031f7c09403c20bdebf75d66
guignont <thomas.guignon@ifpen.fr>
2025-12-17 19:47:53 +0100

    - build with mcgsolver full-gpu ok

--- packages/FindMCGSolver.cmake
+++ packages/FindMCGSolver.cmake
@@ -34,15 +34,7 @@ if(NOT MCGSOLVER_FOUND)
 
     MESSAGE("MCGSOLVER_LIBRARY ${MCGSOLVER_LIBRARY}")
 
-    find_library(MCGSOLVER_CUDA_LIBRARY
-            NAMES MCGSolverCUDA
-            HINTS ${MCGSOLVER_ROOT}
-            PATH_SUFFIXES lib
-            ${_MCGSOLVER_SEARCH_OPTS}
-    )
-    mark_as_advanced(MCGSOLVER_CUDA_LIBRARY)
-
-    find_path(MCGSOLVER_INCLUDE_DIR MCGSolver/LinearSystem/LinearSystem.h
+    find_path(MCGSOLVER_INCLUDE_DIR MCGSolver/MCGSolver.h
             HINTS ${MCGSOLVER_ROOT}
             PATH_SUFFIXES include
             ${_MCGSOLVER_SEARCH_OPTS}
@@ -57,17 +49,15 @@ set(MCGSOLVER_FIND_QUIETLY OFF)
 find_package_handle_standard_args(MCGSOLVER
         DEFAULT_MSG
         MCGSOLVER_INCLUDE_DIR
-        MCGSOLVER_LIBRARY
-        MCGSOLVER_CUDA_LIBRARY)
+        MCGSOLVER_LIBRARY)
 
 if(MCGSOLVER_FOUND AND NOT TARGET mcgsolver)
 
     set(MCGSOLVER_INCLUDE_DIRS ${MCGSOLVER_INCLUDE_DIR})
 
-    set(MCGSOLVER_LIBRARIES ${MCGSOLVER_LIBRARY}
-            ${MCGSOLVER_CUDA_LIBRARY})
+    set(MCGSOLVER_LIBRARIES ${MCGSOLVER_LIBRARY})
 
-    set(MCGSOLVER_FLAGS USE_HWLOC USE_CUDA)
+    set(MCGSOLVER_FLAGS USE_HWLOC)
 
     # mcgsolver main
 
@@ -80,23 +70,12 @@ if(MCGSOLVER_FOUND AND NOT TARGET mcgsolver)
             IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
             IMPORTED_LOCATION "${MCGSOLVER_LIBRARY}")
 
-    # mcgsolver cuda
-
-    add_library(mcgsolver_cuda UNKNOWN IMPORTED)
-
-    set_target_properties(mcgsolver_cuda PROPERTIES
-            IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-            IMPORTED_LOCATION "${MCGSOLVER_CUDA_LIBRARY}")
-
     # mcgsolver
 
     add_library(mcgsolver INTERFACE IMPORTED)
 
     set_property(TARGET mcgsolver APPEND PROPERTY
             INTERFACE_LINK_LIBRARIES "mcgsolver_main")
-
-    set_property(TARGET mcgsolver APPEND PROPERTY
-            INTERFACE_LINK_LIBRARIES "mcgsolver_cuda")
 
     set_target_properties(mcgsolver PROPERTIES
             INTERFACE_COMPILE_DEFINITIONS "${MCGSOLVER_FLAGS}")
